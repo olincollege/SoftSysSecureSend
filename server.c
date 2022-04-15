14efb33c14b5db928c6531fc11b1fbbf991cebaf
DanPark13 <dpark0703@gmail.com>
2022-04-15 01:58:53 -0400

    Basic Server Implementation

--- server.c
+++ server.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-// Hosts client
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 2048
+
+int main(int argc, char **argv){
+	if(argc != 2){
+		printf("Usage: %s <port>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	char *ip = "127.0.0.1";
+	int port = atoi(argv[1]);
+}
