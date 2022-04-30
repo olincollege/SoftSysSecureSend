e9119f346c63c07be871849a897aa04e9fc70b82
DanPark13 <dpark0703@gmail.com>
2022-04-30 00:53:44 -0400

    Annotations

--- src/file_only/client.c
+++ src/file_only/client.c
@@ -12,15 +12,17 @@
 #include <arpa/inet.h>
  
 void send_file(FILE *fp, int sockfd){
-  char data[1024] = {0};
- 
-  while(fgets(data, 1024, fp) != NULL) {
-    if (send(sockfd, data, sizeof(data), 0) == -1) {
-      perror("Error in sending file.");
-      exit(1);
-    }
-    bzero(data, 1024);
-  }
+	char data[1024] = {0};
+
+	// While data is being read
+	while(fgets(data, 1024, fp) != NULL){
+		// Send file through the socket to the server
+		if (send(sockfd, data, sizeof(data), 0) == -1){
+			perror("Error in sending file.");
+			exit(1);
+		}
+	    bzero(data, 1024);
+	}
 }
  
 int main(){
@@ -28,35 +30,42 @@ int main(){
 	int sockfd;
 	struct sockaddr_in server_addr;
 
+	// Declare file contents that is being sent
 	FILE *fp;
 	char *filename = "send.txt";
 
+	// Create Socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0) {
+	if(sockfd < 0){
 		perror("Error in Creating Socket");
 		exit(1);
 	}
 	printf("Server Socket Created Successfully!\n");
 
+	// Connect Socket with listening server
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = 8080;
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	if(e == -1) {
+	if(e == -1){
 		perror("Error in Connecting Socket");
 		exit(1);
 	}
 	printf("Connected to Server Successfully!\n");
 
+	// Open the file
 	fp = fopen(filename, "r");
-	if (fp == NULL) {
+	if (fp == NULL){
 		perror("Error in Reading File");
 		exit(1);
 	}
 
+	// Call send_file to read file and sent to server
 	send_file(fp, sockfd);
 	printf("File Successfully Sent!\n");
+
+	// Close socket
 	close(sockfd);
 	return 0;
 }
--- src/file_only/server.c
+++ src/file_only/server.c
@@ -10,52 +10,58 @@
 #include <string.h>
 #include <arpa/inet.h>
  
-void write_file(int sockfd){
-  int n;
-  FILE *fp;
-  char *filename = "receive.txt";
-  char buffer[1024];
- 
-  fp = fopen(filename, "w");
-  while (1) {
-    n = recv(sockfd, buffer, 1024, 0);
-    if (n <= 0){
-      break;
-      return;
+void receive_file(int sockfd){
+	int n;
+	FILE *fp;
+	char buffer[1024];
+
+	// Create a new file (if not pre-existing)
+	fp = fopen("receive.txt", "w");
+
+	// While data is received
+	while(1){
+		// Receive data through socket
+		n = recv(sockfd, buffer, 1024, 0);
+		if (n <= 0){
+			break;
+			return;
+		}
+		// Save data to file
+		fprintf(fp, "%s", buffer);
+    	bzero(buffer, 1024);
     }
-    fprintf(fp, "%s", buffer);
-    bzero(buffer, 1024);
-  }
-  return;
+    return;
 }
  
 int main(){
-	char *ip = "127.0.0.1";
-	int port = 8080;
 	int e;
 
 	int sockfd, new_sock;
 	struct sockaddr_in server_addr, new_addr;
 	socklen_t addr_size;
 	char buffer[1024];
+
+	// Create socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0) {
+	if(sockfd < 0){
 		perror("Error in Creating Socket");
 		exit(1);
 	}
 	printf("Server Socket Created Successfully!\n");
 
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = port;
-	server_addr.sin_addr.s_addr = inet_addr(ip);
+	server_addr.sin_port = 8080;
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+	// Bind socket to port 8080 and local IP
 	e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	if(e == -1) {
+	if(e == -1){
 		perror("Error in Binding Socket");
 		exit(1);
 	}
 	printf("Successfully Binded to Port!\n");
 
+	// When successful, start listening for connections
 	if(listen(sockfd, 10) == 0){
 		printf("Listening for Client...\n");
 	}
@@ -64,10 +70,12 @@ int main(){
 		exit(1);
 	}
 
+	// Accept connection from client
 	addr_size = sizeof(new_addr);
 	new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
 
-	write_file(new_sock);
+	// Call receive_file to receive data from client through socket
+	receive_file(new_sock);
 	printf("File Received Successfully!\n");
 	return 0;
 }
