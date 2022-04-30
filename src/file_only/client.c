6756f23e0a061d589f4da21766dc7d794b5e2cea
DanPark13 <dpark0703@gmail.com>
2022-04-30 00:35:50 -0400

    Bug Fixes
    - Unnecessary Warning when compiling
    - Socket Creation Message Not Displayed
    - IP Error bounds

--- src/file_only/client.c
+++ src/file_only/client.c
@@ -10,59 +10,53 @@
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
-#define SIZE 1024
  
 void send_file(FILE *fp, int sockfd){
-  char data[SIZE] = {0};
+  char data[1024] = {0};
  
-  while(fgets(data, SIZE, fp) != NULL) {
+  while(fgets(data, 1024, fp) != NULL) {
     if (send(sockfd, data, sizeof(data), 0) == -1) {
       perror("Error in sending file.");
       exit(1);
     }
-    bzero(data, SIZE);
+    bzero(data, 1024);
   }
 }
  
 int main(){
-  char *ip = "127.0.0.1";
-  int port = 8080;
-  int e;
- 
-  int sockfd;
-  struct sockaddr_in server_addr;
-  FILE *fp;
-  char *filename = "send.txt";
- 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(sockfd < 0) {
-    perror("Error in socket");
-    exit(1);
-  }
-  printf("Server socket created successfully.\n");
- 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
- 
-  e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(e == -1) {
-    perror("Error in socket");
-    exit(1);
-  }
- printf("Connected to Server.\n");
- 
-  fp = fopen(filename, "r");
-  if (fp == NULL) {
-    perror("Error in reading file.");
-    exit(1);
-  }
- 
-  send_file(fp, sockfd);
-  printf("File data sent successfully.\n");
- 
-  printf("Closing the connection.\n");
-  close(sockfd);
- 
-  return 0;
+	int e;
+	int sockfd;
+	struct sockaddr_in server_addr;
+
+	FILE *fp;
+	char *filename = "send.txt";
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0) {
+		perror("Error in Creating Socket");
+		exit(1);
+	}
+	printf("Server Socket Created Successfully!\n");
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = 8080;
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if(e == -1) {
+		perror("Error in Connecting Socket");
+		exit(1);
+	}
+	printf("Connected to Server Successfully!\n");
+
+	fp = fopen(filename, "r");
+	if (fp == NULL) {
+		perror("Error in Reading File");
+		exit(1);
+	}
+
+	send_file(fp, sockfd);
+	printf("File Successfully Sent!\n");
+	close(sockfd);
+	return 0;
 }
--- src/file_only/server.c
+++ src/file_only/server.c
@@ -9,66 +9,65 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
-#define SIZE 1024
  
 void write_file(int sockfd){
   int n;
   FILE *fp;
   char *filename = "receive.txt";
-  char buffer[SIZE];
+  char buffer[1024];
  
   fp = fopen(filename, "w");
   while (1) {
-    n = recv(sockfd, buffer, SIZE, 0);
+    n = recv(sockfd, buffer, 1024, 0);
     if (n <= 0){
       break;
       return;
     }
     fprintf(fp, "%s", buffer);
-    bzero(buffer, SIZE);
+    bzero(buffer, 1024);
   }
   return;
 }
  
 int main(){
-  char *ip = "127.0.0.1";
-  int port = 8080;
-  int e;
- 
-  int sockfd, new_sock;
-  struct sockaddr_in server_addr, new_addr;
-  socklen_t addr_size;
-  char buffer[SIZE];
- 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(sockfd < 0) {
-    perror("Error in socket");
-    exit(1);
-  }
-  printf("Server socket created successfully.\n");
- 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
- 
-  e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(e==-1) {
-    perror("Error in bind");
-    exit(1);
-  }
-  printf("Binding successfull.\n");
- 
-  if(listen(sockfd, 10) == 0){
- printf("Listening....\n");
- }else{
- perror("Error in listening");
-    exit(1);
- }
- 
-  addr_size = sizeof(new_addr);
-  new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-  write_file(new_sock);
-  printf("Data written in the file successfully.\n");
- 
-  return 0;
+	char *ip = "127.0.0.1";
+	int port = 8080;
+	int e;
+
+	int sockfd, new_sock;
+	struct sockaddr_in server_addr, new_addr;
+	socklen_t addr_size;
+	char buffer[1024];
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0) {
+		perror("Error in Creating Socket");
+		exit(1);
+	}
+	printf("Server Socket Created Successfully!\n");
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = port;
+	server_addr.sin_addr.s_addr = inet_addr(ip);
+
+	e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if(e == -1) {
+		perror("Error in Binding Socket");
+		exit(1);
+	}
+	printf("Successfully Binded to Port!\n");
+
+	if(listen(sockfd, 10) == 0){
+		printf("Listening for Client...\n");
+	}
+	else{
+		perror("Error in Client Not Found");
+		exit(1);
+	}
+
+	addr_size = sizeof(new_addr);
+	new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
+
+	write_file(new_sock);
+	printf("File Received Successfully!\n");
+	return 0;
 }
