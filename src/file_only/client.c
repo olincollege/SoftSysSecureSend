650caeb5671ae436785296234d6320afe73f1e0c
DanPark13 <dpark0703@gmail.com>
2022-04-30 00:24:53 -0400

    Successful File Transfer
    - Client sends file from directory
    - Server receives file from directory
    - Makefile for easy compilation

--- src/file_only/client.c
+++ src/file_only/client.c
@@ -10,36 +10,59 @@
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
-
+#define SIZE 1024
+ 
 void send_file(FILE *fp, int sockfd){
-	int n;
-	char data[1024] = {0};
-
-	while(fgets(data, 1024, fp) != NULL){
-		if(send(sockfd, data, sizeof(data), 0) == -1){
-			perror("Error in File Transfer.");
-			exit(1);
-		}
-		bzero(data, 1024);
-	}
+  char data[SIZE] = {0};
+ 
+  while(fgets(data, SIZE, fp) != NULL) {
+    if (send(sockfd, data, sizeof(data), 0) == -1) {
+      perror("Error in sending file.");
+      exit(1);
+    }
+    bzero(data, SIZE);
+  }
 }
-
+ 
 int main(){
-	// Declare local IP and port
-	char *ip = "127.0.0.1";
-	int port = 8080;
-	int e;
-
-	// Create socket and connect function is executed
-	int sockfd;
-	struct sockaddr_in server_addr;
-	FILE *fp;
-	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0){
-		perror("Error in Socket Creation.");
-		exit(1);
-	}
-	printf("Server Socket Created Successfully.");
+  char *ip = "127.0.0.1";
+  int port = 8080;
+  int e;
+ 
+  int sockfd;
+  struct sockaddr_in server_addr;
+  FILE *fp;
+  char *filename = "send.txt";
+ 
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) {
+    perror("Error in socket");
+    exit(1);
+  }
+  printf("Server socket created successfully.\n");
+ 
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+ 
+  e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if(e == -1) {
+    perror("Error in socket");
+    exit(1);
+  }
+ printf("Connected to Server.\n");
+ 
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    perror("Error in reading file.");
+    exit(1);
+  }
+ 
+  send_file(fp, sockfd);
+  printf("File data sent successfully.\n");
+ 
+  printf("Closing the connection.\n");
+  close(sockfd);
+ 
+  return 0;
 }
-
